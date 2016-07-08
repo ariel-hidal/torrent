e8fbf4ae8a6e766effe9c82aa63cdad6909a6702
Ariel Hidal <arielhidal@gmail.com>
2016-07-07 20:49:19 -0400

    index now is tutorial version

--- index.js
+++ index.js
@@ -2,12 +2,23 @@
 //required packages
 const fs = require('fs');
 const bencode = require('bencode');
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const urlParse = require('url').parse;
 
-//create torrent IO
-const buffer = fs.readFileSync('puppy.torrent');
-const torrent = bencode.decode(buffer);
+//create url JSON
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+const url = urlParse(torrent.announce.toString('utf8'));
 
-//testing IO
-var announce = torrent.announce.toString('utf8');
-console.log(announce);
 
+//create connection and message
+const socket = dgram.createSocket('udp4');
+const msg = Buffer.from('hello?', 'utf8');
+
+//send message
+socket.send(msg, 0, msg.length, url.port, url.host, () => {});
+
+//receive messages
+socket.on('message', message => {
+  console.log('message is: ' + message);
+});
